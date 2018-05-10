a0613646a9580776a05ad2d4c6248a604b929dc1
Matthew Jones <matt.jones@tvgla.com>
2018-05-10 13:58:47 -0700

    more work

--- wp-content/themes/lhp/index.php
+++ wp-content/themes/lhp/index.php
@@ -2,7 +2,7 @@
 		<section id="content" role="main">
 			index
 		<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-			<?php get_template_part( 'entry' ); ?>
+			<?php get_template_part( 'entry' ); ?>yo
 		<?php endwhile; endif; ?>
 		<?php get_template_part( 'nav', 'below' ); ?>
 		</section>
